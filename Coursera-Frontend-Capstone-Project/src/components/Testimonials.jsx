fdcfe3fa51de436058bd561b93f7b00ca4ee5ddf
Gurpreet Singh <gpsingh02414@gmail.com>
2024-04-05 23:00:00 +0530

    code additon

---
+++ Coursera-Frontend-Capstone-Project/src/components/Testimonials.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Testimonials() {
+  return (
+    <div className="testimonialWrapper">
+      <div className="name">John Doe</div>
+      <div className="paragraph">
+        Good place to eat and enjoy your favourite time with your family and
+        friends
+      </div>
+    </div>
+  );
+}

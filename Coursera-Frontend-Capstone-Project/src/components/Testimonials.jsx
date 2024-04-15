553b1dfd21181860823e5d2faf99477464234453
Gurpreet Singh <gpsingh02414@gmail.com>
2024-04-15 23:00:00 +0530

    code updated

--- Coursera-Frontend-Capstone-Project/src/components/Testimonials.jsx
+++ Coursera-Frontend-Capstone-Project/src/components/Testimonials.jsx
@@ -1,12 +1,26 @@
-import React from "react";
-
+// export default function Testimonials() {
+//   return (
+//     <div classNameName="testimonialWrapper">
+//       <div classNameName="name">John Doe</div>
+//       <div classNameName="paragraph">
+//         Good place to eat and enjoy your favourite time with your family and
+//         friends
+//       </div>
+//     </div>
+//   );
+// }
 export default function Testimonials() {
   return (
     <div className="testimonialWrapper">
-      <div className="name">John Doe</div>
-      <div className="paragraph">
-        Good place to eat and enjoy your favourite time with your family and
-        friends
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">John Doe</h5>
+
+          <p className="card-text">
+            Good place to eat and enjoy your favourite time with your family and
+            friends
+          </p>
+        </div>
       </div>
     </div>
   );

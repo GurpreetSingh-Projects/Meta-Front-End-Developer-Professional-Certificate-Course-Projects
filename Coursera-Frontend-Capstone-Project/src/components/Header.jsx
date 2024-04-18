26234f005dac0d7ef8b30979e9d800e63a721c65
Gurpreet Singh <gpsingh02414@gmail.com>
2024-04-18 23:00:00 +0530

    code updated

--- Coursera-Frontend-Capstone-Project/src/components/Header.jsx
+++ Coursera-Frontend-Capstone-Project/src/components/Header.jsx
@@ -1,15 +1,69 @@
 import Nav from "./Nav";
 export default function Header() {
   return (
-    <div>
-      <div className="headerContainer">
-        <div className="left">
-          <img src="assets/img/logo.png" alt="hi" />
-        </div>
-        <div className="right">
-          <Nav />
+    <nav className="navbar navbar-expand-lg bg-body-tertiary headerContainer">
+      <div className="container-fluid p-5">
+        <a className="navbar-brand" href="#">
+          <img
+            src={"assets/img/logo.png"}
+            width="100%"
+            height="30"
+            className="p-2"
+            alt=""
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Menu
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Reservations
+              </a>
+            </li>{" "}
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Order
+              </a>
+            </li>{" "}
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
         </div>
       </div>
-    </div>
+    </nav>
   );
 }

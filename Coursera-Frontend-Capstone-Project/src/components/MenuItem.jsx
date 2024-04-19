cc1662f454f94d66b9aec111e670d48210c16f04
Gurpreet Singh <gpsingh02414@gmail.com>
2024-04-19 23:00:00 +0530

    code updated

--- Coursera-Frontend-Capstone-Project/src/components/MenuItem.jsx
+++ Coursera-Frontend-Capstone-Project/src/components/MenuItem.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+// import React from "react";
+
+// export default function MenuItem({ img, name, price }) {
+//   return (
+//     <section classNameName="menuItem">
+//       <img src={img} alt="" />
+//       <div classNameName="title">{name}</div>
+//       <div classNameName="description">{price} Rs</div>
+//     </section>
+//   );
+// }
 
 export default function MenuItem({ img, name, price }) {
   return (
-    <section className="menuItem">
-      <img src={img} alt="" />
-      <div className="title">{name}</div>
-      <div className="description">{price} Rs</div>
-    </section>
+    <div className="card m-2 menuItem">
+      <img src={img} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{price}</p>
+      </div>
+    </div>
   );
 }

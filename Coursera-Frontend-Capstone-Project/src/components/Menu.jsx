d6db7d1cf38108e1eb19643a36fa095545b56d26
Gurpreet Singh <gpsingh02414@gmail.com>
2024-04-20 23:00:00 +0530

    code updated

--- Coursera-Frontend-Capstone-Project/src/components/Menu.jsx
+++ Coursera-Frontend-Capstone-Project/src/components/Menu.jsx
@@ -1,22 +1,84 @@
-import React from "react";
-
+import slide1 from "../../public/assets/img/slide1.jpg";
+import slide2 from "../../public/assets/img/slide2.jpg";
+import slide3 from "../../public/assets/img/slide3.jpg";
 export default function Menu() {
   return (
-    <section id="hero">
-      <div className="left">
-        <div className="title">
-          Little Lemon <br /> Chicago
-        </div>
-        <div className="para">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-          placeat commodi totam debitis beatae odio labore ipsa iure atque
-          provident.
-        </div>
-        <button className="reserveButton">Reserve Table</button>
+    <div id="carouselExampleCaptions" className="carousel slide">
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
       </div>
-      <div className="right">
-        <img src="assets/img/heroimg.jpg" alt="" />
+      <div
+        className="carousel-inner overflow-hidden"
+        style={{ height: "90vh", zIndex: "-1" }}
+      >
+        <div className="carousel-item active">
+          <img src={slide1} className="img-fluid d-block" alt="slide" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Pan Cakes</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+              inventore!
+            </p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src={slide2} className="img-fluid d-block w-100 " alt="slide" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Tasty Food</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+              inventore!
+            </p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src={slide3} className="img-fluid d-block w-100" alt="slide" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Blueberry Dessert</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+              inventore!
+            </p>
+          </div>
+        </div>
       </div>
-    </section>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
   );
 }
